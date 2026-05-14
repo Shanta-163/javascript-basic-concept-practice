2ef11c8e440250ec9c0f774a397d29979ded4e92
Shanta-163 <farhana.ys811@gmail.com>
2026-05-14 06:32:18 +0600

    Add files via upload
    DOM recap added

---
+++ DOM recap/recap.js
@@ -0,0 +1,18 @@
+/* how to use js with html and js
+2. how to get element or elements from dom
+2-1.getElementById
+2-2.getElementsByClassName
+2-3.querySelector
+2-4.querySelectorAll 
+3. dynamically add element to the dom: documnet.createElement
+3-5.set value to the innerHTML
+4. get and set value from dom
+4-1. use innerText (h1,p,span,strong,div)
+4-2.use value(input,textArea)
+5. add event listener(event handler)
+5-1. click, focus,blur,keyup,onchange
+5-2. addEventListener onclick = "myHandler()"
+6.use function to reduce duplicate code
+7.Event bubble and event delegate*/
+document.querySelectorAll(".users-contain .disables h3")
+document.querySelectorAll(".user p")
---
+++ DOM recap/validation.js
@@ -0,0 +1,12 @@
+const num = 23;
+//string , number, boolean
+if(typeof num === 'number'){
+    console.log('value is a number');
+}
+else{
+    console.log('value is not a number');
+}
+const student = {name:'fatema',job:'web developer'}
+const number = [2,3,5,8];
+console.log(Array.isArray(number));
+console.log(typeof  student)
